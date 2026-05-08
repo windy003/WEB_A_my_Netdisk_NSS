4b949aa3c166d2653c376b31e8e38ef764c05df7
windy003 <928920471@qq.com>
2026-05-08 13:28:13 +0800

    ok

--- run_myNetdisk_webserver.cmd
+++ run_myNetdisk_webserver.cmd
@@ -1,1 +1,1 @@
-start /b pythonw "D:\files\using\Web\my_Netdisk_NSS\app.py"
+start /b pythonw "D:\files\using\Web\WEB_A_my_Netdisk_NSS\app.py"
--- x13y-gen2-my_netdisk_sync_to_gdrive.py.bat
+++ x13y-gen2-my_netdisk_sync_to_gdrive.py.bat
@@ -1,2 +1,2 @@
-start  /B  pythonw "D:\files\using\Web\my_Netdisk_NSS\x13y-gen2-my_netdisk_sync_to_gdrive.py"
+start  /B  pythonw "D:\files\using\Web\WEB_A_my_Netdisk_NSS\x13y-gen2-my_netdisk_sync_to_gdrive.py"
 
